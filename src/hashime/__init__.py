c0d29d8554111201802e9148b8d175f87188969d
sakhezech <contact@sakhezech.ru>
2024-02-15 23:11:55 +0300

    expose frame and add file_digest helper func

--- src/hashime/__init__.py
+++ src/hashime/__init__.py
@@ -1,4 +1,5 @@
 from hashime.drunken_bishop import drunken_bishop
 from hashime.fish_tank import fish_tank
+from hashime.util import file_digest, frame
 
-__all__ = ['drunken_bishop', 'fish_tank']
+__all__ = ['drunken_bishop', 'fish_tank', 'file_digest', 'frame']
--- src/hashime/util.py
+++ src/hashime/util.py
@@ -1,3 +1,9 @@
+import hashlib
+from os import PathLike
+
+StrPath = PathLike[str] | str
+
+
 def clamp(val: int, min_: int, max_: int) -> int:
     return min(max(val, min_), max_)
 
@@ -9,6 +15,11 @@ def bit_set_in_pos(num: int, pos: int) -> int:
 def bits_set_in_range(num: int, start: int, end: int) -> int:
     mask = (2 ** (end - start)) - 1
     return (num >> start) & mask
+
+
+def file_digest(fp: StrPath, hash_function: str = 'sha256') -> bytes:
+    with open(fp, 'rb') as file:
+        return hashlib.file_digest(file, hash_function).digest()
 
 
 def frame(art: str) -> str:

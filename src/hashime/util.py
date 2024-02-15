8d2fe79c6a6b96fa15763b5609be32e4b7919c5d
sakhezech <contact@sakhezech.ru>
2024-02-16 01:44:57 +0300

    new frame func

--- src/hashime/util.py
+++ src/hashime/util.py
@@ -22,9 +22,33 @@ def file_digest(fp: StrPath, hash_function: str = 'sha256') -> bytes:
         return hashlib.file_digest(file, hash_function).digest()
 
 
-def frame(art: str) -> str:
-    lines = art.split('\n')
-    width = len(lines[0])
-    horizontal_line = f'+{"-" * width}+'
-    content = '\n'.join([f'|{line}|' for line in lines])
-    return '\n'.join([horizontal_line, content, horizontal_line])
+def frame(
+    art: str,
+    top_text: str | None = None,
+    bottom_text: str | None = None,
+    brackets: tuple[str, str] = ('[', ']'),
+    lines: tuple[str, str, str, str] = ('-', '|', '-', '|'),
+    corners: tuple[str, str, str, str] = ('+', '+', '+', '+'),
+) -> str:
+    art_lines = art.split('\n')
+    width = len(art_lines[0])
+
+    # unpacking the values from tuples
+    frame_t, frame_r, frame_b, frame_l = lines
+    corner_tl, corner_tr, corner_br, corner_bl = corners
+    bracket_l, bracket_r = brackets
+
+    if top_text is None:
+        top_text = ''
+    else:
+        top_text = f'{bracket_l}{top_text}{bracket_r}'
+
+    if bottom_text is None:
+        bottom_text = ''
+    else:
+        bottom_text = f'{bracket_l}{bottom_text}{bracket_r}'
+
+    top_line = top_text.center(width, frame_t).join((corner_tl, corner_tr))
+    bot_line = bottom_text.center(width, frame_b).join((corner_bl, corner_br))
+    content = '\n'.join(f'{frame_l}{line}{frame_r}' for line in art_lines)
+    return '\n'.join((top_line, content, bot_line))

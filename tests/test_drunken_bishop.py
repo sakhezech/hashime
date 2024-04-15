2129da2623cf180311bb0ac8b453a8dc347037fb
sakhezech <contact@sakhezech.ru>
2024-04-16 01:56:23 +0300

    pretty indented test

--- tests/test_drunken_bishop.py
+++ tests/test_drunken_bishop.py
@@ -1,5 +1,6 @@
 import base64
 import hashlib
+import textwrap
 
 import hashime
 import pytest
@@ -12,26 +13,28 @@ def key_to_digest(key: str) -> bytes:
 
 
 key1 = 'AAAAC3NzaC1lZDI1NTE5AAAAIGAaC/j/5r5xDP+1fECB0zOS705VV8b41WHy5VMO6Xp2'
-art1 = """        o.*+ .   
-       + * oo.   
-      o = = +    
- .   .   * =     
-  o  .  S o.+    
-.. ....   oo     
-.. E+ ..ooo      
-  o.oo oo=oo     
- ..o +*+==+      """
+art1 = textwrap.dedent("""\
+                o.*+ .   
+               + * oo.   
+              o = = +    
+         .   .   * =     
+          o  .  S o.+    
+        .. ....   oo     
+        .. E+ ..ooo      
+          o.oo oo=oo     
+         ..o +*+==+      """)
 
 key2 = 'AAAAC3NzaC1lZDI1NTE5AAAAIJWirJiqZR0H2M4h6W9bRVGa3ZbvtGhE5R/V6FIX58Xo'
-art2 = """    ..           
-   ..            
-   ..     .      
-  .  .   . o    .
-o.o . . S o +.o o
-oo *   . o B.=oo+
-  . +   + +.Boo=.
-   o.o.o . Bo.. o
-  .o=Eo.  oo=.   """
+art2 = textwrap.dedent("""\
+            ..           
+           ..            
+           ..     .      
+          .  .   . o    .
+        o.o . . S o +.o o
+        oo *   . o B.=oo+
+          . +   + +.Boo=.
+           o.o.o . Bo.. o
+          .o=Eo.  oo=.   """)
 
 
 @pytest.mark.parametrize('key, art', [(key1, art1), (key2, art2)])
